c2bdc2e1aa5bf9032ea85b2877fca0cae2fce5fb
duanchi <07.duanchi@gmail.com>
2015-03-16 00:12:00 +0800

    add data_abstract

---
+++ library/Data/__Abstract.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lovemybud
+ * Date: 15/3/15
+ * Time: 23:18
+ */
+
+namespace Data;
+
+
+class __Abstract {
+    static public function get () {
+
+    }
+}
