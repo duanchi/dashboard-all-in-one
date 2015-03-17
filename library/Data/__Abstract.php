822d59fd8bfa9904d9f41940269f2c1d2e01c829
duanchi <07.duanchi@gmail.com>
2015-03-17 18:30:35 +0800

    03-17

--- library/Data/__Abstract.php
+++ library/Data/__Abstract.php
@@ -10,7 +10,23 @@ namespace Data;
 
 
 class __Abstract {
-    static public function get () {
+    static public function get ($_key) {
+
+    }
+
+    static public function set ($_key, $_value) {
+
+    }
+
+    static public function __get($_key) {
+
+    }
+
+    static public function __set($_key, $_value) {
+
+    }
+
+    static protected function get_data_file() {
 
     }
 }
