f6a2bc44c835dadbc3785a241223be9567270696
duanchi <duanchi@b1703.devel>
2015-06-02 17:45:43 +0800

    add nodejs framework support

--- client/app/core/dispatcher.core.js
+++ client/app/core/dispatcher.core.js
@@ -9,5 +9,14 @@ module.exports  =   function(App) {
         return App.conf;  
     };
     
+    this.get_route      =   function() {
+        return {
+            'module':       App.request.module,
+            'controller':   App.request.cotroller,
+            'action':       App.request.action
+            
+        }
+    };
+    
     return this;
 }
