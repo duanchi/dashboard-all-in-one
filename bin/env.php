655d8b20725fc8f2c6a65db8a63c6a355394eb67
duanchi <07.duanchi@gmail.com>
2015-07-10 11:09:11 +0800

    07-10

--- bin/env.php
+++ bin/env.php
@@ -5,7 +5,7 @@
  * Date: 15/7/5
  * Time: 上午10:10
  */
-echo time();
-//phpinfo();
-
-var_dump(Yaconf::get('1436369968_common_base'));
+//var_dump(get_loaded_extensions());
+phpinfo();
+//var_dump(get_defined_constants(true));
+//var_dump(Yaconf::get('1436369968_common_base'));
